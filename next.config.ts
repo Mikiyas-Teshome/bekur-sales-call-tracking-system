354297738f6adc436d9c4cc6dde95df08a17fcf0
Mikiyas-Teshome <ewenetmikiyas@gmail.com>
2026-09-18 12:15:54 +0300

    feat: add PostgreSQL package to server external packages in configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  serverExternalPackages: ["typeorm"],
+  serverExternalPackages: ["typeorm", "pg"],
 };
 
 export default nextConfig;
